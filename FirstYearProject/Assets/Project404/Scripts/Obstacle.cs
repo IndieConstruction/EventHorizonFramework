b8b4b2a0c5c1aac10c691e6affcdc0c2dc80cd9d
Viviana Figus <viviana.figus@gmail.com>
2016-04-14 12:32:04 +0200

    Creati un ItemSpawner e un'interfaccia IItem per il FrameWork

---
+++ FirstYearProject/Assets/Project404/Scripts/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using EH.FrameWork;
+namespace EH.Project404{
+public class Obstacle : MonoBehaviour, IItem {
+
+		GameObject obstacle;
+		public GameObject ItemGameObject {
+			get{
+				return obstacle;
+				}
+			set{
+				obstacle = this.gameObject;
+				}
+			}
+		/// <summary>
+		/// Raises the trigger enter event.
+		/// </summary>
+		/// <param name="other">Other.</param>
+		public void OnTriggerEnter ( Collider other){
+
+		}
+}
+}
