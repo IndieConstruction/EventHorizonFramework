73d112dcd4cc15ee72cb8f141c2a0cabec404d9f
Amir Basyouni <Amir Basyouni>
2016-04-17 00:11:34 +0200

    Le 2 liste ciclano. p.s odio le liste. p.s2 non potevamo farlo con un bell array[] ?? Vado a dormire nervoso

--- FirstYearProject/Assets/FirstyearProjectAmir/Scripts/RoadManager.cs
+++ FirstYearProject/Assets/FirstyearProjectAmir/Scripts/RoadManager.cs
@@ -10,9 +10,9 @@ public class RoadManager : MonoBehaviour   {
 	/// </summary>
 	public int MaxRoadsInGame = 6;
 	/// <summary>
-	/// Lista gameobject totali.
+	/// Lista gameobject totali random da prendere dalla lista delle texture.
 	/// </summary>
-	public List <GameObject> Prefabs = new List<GameObject>();
+	public List <GameObject> RoadsPrefabs = new List<GameObject>();
 
 	float RoadLenght = 10;
 	/// <summary>
@@ -31,6 +31,7 @@ public class RoadManager : MonoBehaviour   {
 	void update(){
 
 	}
+
 	void fixedUpdate(){
 
 	}
@@ -46,10 +47,25 @@ public class RoadManager : MonoBehaviour   {
 		Vector3 ActualRoadPosition = objectToReposition.transform.position;
 		objectToReposition.transform.position =new Vector3 (ActualRoadPosition.x,ActualRoadPosition.y,ActualRoadPosition.z + (RoadLenght*ListRoadsLenght));
 	}
+	/// <summary>
+	/// Cicla la liste delle Road e le aggiunge al contatore
+	/// </summary>
+	/// <param name="roadsToAdd">Roads to add.</param>
+	public void RoadsCounter(GameObject roadsToAdd){
+		foreach (var item in Roads) {
+			Debug.Log("Sto ciclando e aggiungo cose");
+			Roads.Add (roadsToAdd);
+		}
 
-	public void RoadsCounter(){
-		foreach (var GameObject in Roads) {
-			MaxRoadsInGame ++;
+	}
+	/// <summary>
+	/// Cicla la lista delle RoadTexture e le aggiunge al contatore
+	/// </summary>
+	/// <param name="roadsPrefabToAd">Roads prefab to ad.</param>
+	public void RandomSpawnRoadTexture(GameObject roadsPrefabToAdd){
+		for (int i = 0; i < RoadsPrefabs.Count; i++) {
+			RoadsPrefabs.Add (roadsPrefabToAdd);
 		}
 	}
+	
 }
