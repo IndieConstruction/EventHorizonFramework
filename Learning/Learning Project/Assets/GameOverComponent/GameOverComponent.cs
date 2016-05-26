89822c1c3440e001625383b599686da8b086de43
Brago <bragolab@gmail.com>
2016-05-26 12:18:45 +0200

    Creato proggetto learning

---
+++ Learning/Learning Project/Assets/GameOverComponent/GameOverComponent.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverComponent : MonoBehaviour {
+
+    private bool isActive;
+
+    public bool IsActive
+    {
+        get { return isActive; }
+        set {
+            //if(isActive != value) {
+                if (State == GameOverState.Lost)
+                {
+                    TitleText.text = "You Lost!!";
+                    NextLevelButton.gameObject.SetActive(false);
+                    RestartLevelButton.gameObject.SetActive(true);
+                }
+                else {
+                    TitleText.text = "You Win!!";
+                    NextLevelButton.gameObject.SetActive(true);
+                    RestartLevelButton.gameObject.SetActive(false);
+                }
+                GetComponent<Animator>().SetBool("IsActive", isActive);
+            //}
+            isActive = value;
+            
+        }
+    }
+
+    public enum GameOverState { Win, Lost }
+    public GameOverState State;
+    public Text TitleText;
+    public Button NextLevelButton;
+    public Button RestartLevelButton;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Logica
+    public void SetWindowVisible(bool _visible) {
+        IsActive = _visible;
+    }
+}
---
+++ Learning/Learning Project/Assets/PlayerPrefs/PlayerPrefsTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerPrefsTest : MonoBehaviour {
+
+    public InputField NameInput;
+    public Text NameLable;
+
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.GetString("MyName") != null) {
+            NameLable.text = PlayerPrefs.GetString("MyName") + " Lev: " + PlayerPrefs.GetInt("Level");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+            
+            
+	}
+
+    public void SaveProfile() {
+        PlayerPrefs.SetString("MyName", NameInput.text);
+        PlayerPrefs.SetInt("Level", 1);
+        PlayerPrefs.Save();
+    }
+}
